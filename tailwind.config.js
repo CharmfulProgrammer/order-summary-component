fc7f69eac80b8e47c7f7b41a2c78d1a42cfc6ec2
NajeebFarhan <54494073+CharmfulProgrammer@users.noreply.github.com>
2021-08-15 15:52:38 +0530

    unstyled project

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,28 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      "pale-blue": "hsl(225, 100%, 94%)",
+      "bright-blue": "hsl(245, 75%, 52%)",
+      "very-pale-blue": "hsl(225, 100%, 98%)",
+      "desaturated-blue": "hsl(224, 23%, 55%)",
+      "dark-blue": "hsl(223, 47%, 23%)",
+    },
+    fontSize: {
+      paragraph: "16px",
+    },
+    fontFamily: {
+      "red-hat": ["'Red Hat Display'", "sans-serif"],
+    },
+    screens: {
+      "mobile": "375px"
+    },
+    zIndex: {
+      "background": "-9999"
+    }
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
