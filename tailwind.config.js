aaef7859c2cab494edef27e9126b0437b23337f6
NajeebFarhan <54494073+CharmfulProgrammer@users.noreply.github.com>
2021-08-16 09:52:14 +0530

    finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,32 @@
 module.exports = {
-  purge: [],
+  mode: "jit",
+  purge: [
+    "./index.html"
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        paragraph: "16px",
+      }
+    },
+    fontFamily: {
+      "red-hat": ["'Red Hat Display'", "sans-serif"],
+    },
     colors: {
       "pale-blue": "hsl(225, 100%, 94%)",
       "bright-blue": "hsl(245, 75%, 52%)",
       "very-pale-blue": "hsl(225, 100%, 98%)",
       "desaturated-blue": "hsl(224, 23%, 55%)",
       "dark-blue": "hsl(223, 47%, 23%)",
-    },
-    fontSize: {
-      paragraph: "16px",
-    },
-    fontFamily: {
-      "red-hat": ["'Red Hat Display'", "sans-serif"],
+      "white": "#fff"
     },
     screens: {
-      "mobile": "375px"
+      mobile: "375px",
     },
     zIndex: {
-      "background": "-9999"
-    }
+      background: "-9999",
+    },
   },
   variants: {
     extend: {},
